1a1ec520817b2775581f37ff9fd776594662e93d
AndreasDeceuninck <r0894673@student.vives.be>
2023-03-15 11:16:50 +0100

    eerste commit

---
+++ broodjeszaak/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.Models;
+
+namespace Orders.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult order()
+        {
+            return View(new Order());
+        }
+
+        [HttpPost]
+        public ViewResult order(Order orders, Repository repository)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.AddOrder(orders);
+                var order = Repository.Orders.Last();
+                return View("show", order);
+            }
+            else
+            {
+                return View(new Order());
+            }
+        }
+
+        public ViewResult ListOrders()
+        {
+            var orders = Repository.Orders;
+            return View(orders);
+        }
+    }
+}
